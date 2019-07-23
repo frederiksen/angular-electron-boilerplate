fd88d484ea839b61a3ef69269294ec5893450dcd
Morten Frederiksen <morten@myfunkyapps.com>
2019-07-23 16:47:58 +0200

    Add electron main files

---
+++ src/main/main.ts
@@ -0,0 +1,39 @@
+import { app, BrowserWindow, ipcMain } from 'electron';
+import * as path from 'path';
+
+let win: BrowserWindow;
+
+app.on('ready', createWindow);
+
+app.on('activate', () => {
+  if (win === null) {
+    createWindow();
+  }
+});
+
+function createWindow() {
+  win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      // Disabled nodeIntegration
+      nodeIntegration: false,
+      // In a sandbox
+      sandbox: true,
+      // JavaScript context isolation for preload scripts
+      contextIsolation: true,
+      // Preload script
+//      preload: path.join(app.getAppPath(), 'dist/main', 'preload.js')
+    }
+  });
+
+  win.loadFile(path.join(app.getAppPath(), 'dist/renderer', 'index.html'));
+
+  win.on('closed', () => {
+    win = null;
+  });
+}
+
+ipcMain.on('app.quit', (event, arg) => {
+  app.quit();
+});
---
+++ src/main/webpack.js
@@ -0,0 +1,38 @@
+const path = require('path')
+
+module.exports = env => {
+  if (!env) { env = "development" };
+  return {
+    entry: {
+      main: "./src/main/main.ts"
+    },
+    target: "electron-main",
+    output: {
+      path: path.resolve(__dirname, '../../dist/main'),
+      filename: 'electron-main.js'
+    },
+    externals: [ ],
+    devtool: "source-map",
+    module: {
+      rules: [
+        {
+          test: /\.ts?$/,
+          loader: 'ts-loader',
+          exclude: /node_modules/
+        }
+      ]
+    },
+    resolve: {
+      extensions: ['.js', '.ts', '.tsx', '.jsx', '.json'],
+      alias: {
+        // TODO
+      }
+    },
+    node: {
+      __dirname: true,
+      __filename: true
+    },
+    plugins: [
+    ]
+  }
+}
