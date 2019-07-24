588955286c857fb8de7389edab4650c3a3ce3fb5
Morten Frederiksen <morten@myfunkyapps.com>
2019-07-24 15:28:32 +0200

    Material

--- src/renderer/src/app/app.component.ts
+++ src/renderer/src/app/app.component.ts
@@ -3,44 +3,11 @@ import { DtoSystemInfo } from '../../../ipc-dtos/dtosysteminfo';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <!--The content below is only a placeholder and can be replaced.-->
-    <div style="text-align:center">
-      <h1>
-        Welcome to {{title}}!
-      </h1>
-    </div>
-    <h2>Here are some links to help you start: </h2>
-    <button (click)="onClickMe()">Click me!</button>
-    <h3>{{os}}</h3>
-    <h3>{{mem}}</h3>
-
-    <router-outlet></router-outlet>
-  `,
-  styles: []
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'renderer';
-
-  os = '-';
-  mem = 0;
-
   constructor() {
-    window.electronIpcOn('test', (event: any, arg: any) => {
-      console.log('got an electron ipc event');
-      const systemInfo = DtoSystemInfo.deserialize(arg);
-
-      console.log(systemInfo.Os);
-      console.log(systemInfo.Mem);
-
-      this.os = systemInfo.Os;
-      this.mem = systemInfo.Mem;
-    });
   }
-
-  onClickMe(): void {
-    console.log('button clicked');
-    window.electronIpcSend('app-quit');
-  }
-
 }
--- src/renderer/src/app/app.module.ts
+++ src/renderer/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { MaterialModule } from './material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
