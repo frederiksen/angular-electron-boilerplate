d446fb6317b2f2e28163509f36d953901c3136e5
Morten Frederiksen <morten@myfunkyapps.com>
2019-07-24 21:12:51 +0200

    Cleanup

--- src/renderer/src/app/app-routing.module.ts
+++ src/renderer/src/app/app-routing.module.ts
@@ -2,14 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { Component1Component } from './components/component1/component1.component';
 import { Component2Component } from './components/component2/component2.component';
-import { Component3Component } from './components/component3/component3.component';
-import { Component4Component } from './components/component4/component4.component';
 
 const routes: Routes = [
   { path:  '', component:  Component1Component },
   { path:  '2', component:  Component2Component },
-  { path:  '3', component:  Component3Component },
-  { path:  '4', component:  Component4Component }
 //  { path: '404', component: NotfoundComponent },
 //  { path: '**', redirectTo: '/404' }
 ];
--- src/renderer/src/app/app.module.ts
+++ src/renderer/src/app/app.module.ts
@@ -7,8 +7,6 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { Component1Component } from './components/component1/component1.component';
 import { Component2Component } from './components/component2/component2.component';
-import { Component3Component } from './components/component3/component3.component';
-import { Component4Component } from './components/component4/component4.component';
 import { NavlistComponent } from './components/navlist/navlist.component';
 
 @NgModule({
@@ -16,8 +14,6 @@ import { NavlistComponent } from './components/navlist/navlist.component';
     AppComponent,
     Component1Component,
     Component2Component,
-    Component3Component,
-    Component4Component,
     NavlistComponent
   ],
   imports: [
