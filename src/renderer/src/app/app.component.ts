180d16bd3dba7a27bb2f97cbe687b0992dac7396
Morten Frederiksen <morten@myfunkyapps.com>
2019-07-24 18:11:52 +0200

    Material design done

--- src/renderer/src/app/app.component.ts
+++ src/renderer/src/app/app.component.ts
@@ -7,7 +7,7 @@ import { DtoSystemInfo } from '../../../ipc-dtos/dtosysteminfo';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'renderer';
+  title = 'angular-electron-boilerplate';
   constructor() {
   }
 }
