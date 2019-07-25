f959e57cf734225d999231c1c88abde09c000562
Morten Frederiksen <morten@myfunkyapps.com>
2019-07-25 16:11:22 +0200

    Add missing types

--- src/renderer/src/app/app.component.spec.ts
+++ src/renderer/src/app/app.component.spec.ts
@@ -20,10 +20,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'renderer'`, () => {
+  it(`should have as title 'angular-electron-boilerplate'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('renderer');
+    expect(app.title).toEqual('angular-electron-boilerplate');
   });
 
   it('should render title in a h1 tag', () => {
