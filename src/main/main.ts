4e80a286bb88db881cff728ace823c7d19474b29
Andreas Oehlke <andreas.oehlke@twistedbytes.net>
2021-01-18 02:51:53 +0100

    Add Hot-Reloading for Renderer and Renderer+Main
    I added two new scripts that are meant to be run by devs:
    - start:dev:hot-reloading:renderer+main
    - start:dev:hot-reloading:renderer-only
    
    I also changed the 'start' script to default to the new
    'renderer+main' script because this is probably the best user
    experience while developing your application.
    
    There are three more new scripts that are only for internal use.
    Maybe these should be prefixed with an underscore or hash?
    
    How it works
    ------------
    
    Renderer:
    Webpack will watch the renderer source directory and rebuild files as
    needed which will also results in a hot-reload inside the Angular app.
    
    Renderer+Main:
    In addition to the behavior described above, the running Electron
    process will be watched using the 'ElectronReloadPlugin' plugin.
    The plugin will kill and respawn Electron whenever files in the main
    source directory are changed.
    
    In both cases Electron is launched in parallel to the rest of the
    application and will wait until port 4200/TCP is open. Through
    this network port Angular is able to communicate with the WDS
    (Webpack Dev Server).
    
    Depending on the selected script (Renderer or Renderer+Main),
    Electron will simply launch and use the files in dist/main or
    it will be killed and respawned by the reloader plugin as needed.
    
    The implementation of the Renderer script contains a little QoL
    feature which takes care of shutting down the renderer watcher
    as soon as the Electron process has shutdown. Unfortunately,
    this is not the case with the Renderer+Main script. The user will
    have to stop the process manually (e.g. Ctrl-C) for now.

--- src/main/main.ts
+++ src/main/main.ts
@@ -3,6 +3,8 @@ import * as path from 'path';
 import { DtoSystemInfo } from '../ipc-dtos/dtosysteminfo';
 import * as os from 'os';
 
+const BRING_WINDOW_TO_FRONT_ON_START = true;
+
 let win: BrowserWindow | null = null;
 
 app.on('ready', createWindow);
@@ -13,7 +15,7 @@ app.on('activate', () => {
   }
 });
 
-function createWindow() {
+async function createWindow(): Promise<void> {
   win = new BrowserWindow({
     width: 800,
     height: 600,
@@ -26,13 +28,34 @@ function createWindow() {
       enableRemoteModule: false,
       // Preload script
       preload: path.join(app.getAppPath(), 'dist/preload', 'preload.js')
-    }
+    },
+    show: false, // Will be made visible after page has been fully loaded for the first ime
   });
 
   // https://stackoverflow.com/a/58548866/600559
   Menu.setApplicationMenu(null);
 
-  win.loadFile(path.join(app.getAppPath(), 'dist/renderer', 'index.html'));
+  win.once("ready-to-show", () => {
+    if (win) {
+      win.show();
+      if (BRING_WINDOW_TO_FRONT_ON_START) {
+		// Prevent ugly flickering
+        setTimeout(() => {
+          win?.minimize();
+          win?.restore();
+        }, 10);
+      }
+    }
+  });
+
+  if (process.env.WEBPACK_DEV_SERVER_URL) {
+    // Load the url of the dev server if in development mode
+    await win.loadURL(process.env.WEBPACK_DEV_SERVER_URL.trim());
+  } else {
+    await win.loadFile(
+      path.join(app.getAppPath(), "dist/renderer", "index.html")
+    );
+  }
 
   win.on('closed', () => {
     win = null;
